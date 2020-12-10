162b7dee0dbebd14b6d916c54e077ad7176fc4ea
ggaldino95 <ggaldino95@gmail.com>
2020-12-10 15:03:49 -0300

    [Task 1] Completed - Created container to receive text and image for meme cgeneration

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-window.onload = function () {
+function generateTextInputBox() {
+  const container = document.getElementById('inputs');
+  const textBox = document.createElement('input');
+  textBox.type = 'text';
+  textBox.id = 'text-input';
+  textBox.className = 'text-input';
+  textBox.placeholder = 'Type your meme text here';
 
+  container.appendChild(textBox);
+}
+
+function generateMemeText() {
+  const container = document.getElementById('meme-image-container');
+  const memeText = document.createElement('p');
+  memeText.id = 'meme-text';
+  memeText.className = 'meme-text';
+  memeText.innerText = 'Test text';
+
+  container.appendChild(memeText);
+}
+
+window.onload = function () {
+  generateTextInputBox();
+  generateMemeText();
 };
