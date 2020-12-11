c5c76ed4cc969e0b084aa0a043f1e79a9fa722cd
Tiago Yoneda <xyonedax@gmail.com>
2020-12-10 23:23:41 -0300

    colocando coisas extras

--- script.js
+++ script.js
@@ -16,7 +16,11 @@ function generateInputSection() {
   const imageInput = document.createElement('input');
   imageInput.type = 'file';
   imageInput.id = 'meme-insert';
+  imageInput.className = 'img-btn';
   imageInput.accept = 'image/png, image/jpeg';
+  imageInput.addEventListener('change', function(self){
+    document.querySelector('#meme-image').src = self.target.value;
+  })
   document.querySelector('.inputContainer').appendChild(imageInput);
 }
 
@@ -38,3 +42,51 @@ generateMemeSection();
 document.querySelector('#text-input').addEventListener('input',function (self) {
   document.querySelector('#meme-text').innerText = self.target.value;
 });
+
+// importado do stackoverflow
+// https://stackoverflow.com/questions/31236172/change-an-image-from-a-file-input-without-sending-the-form
+
+var inp = document.querySelector('#meme-insert');
+inp.addEventListener('change', function(e){
+    var file = this.files[0];
+    var reader = new FileReader();
+    reader.onload = function(){
+        document.querySelector('#meme-image').removeAttribute('style');
+        document.getElementById('meme-image').src = this.result;
+        };
+    reader.readAsDataURL(file);
+    },false);
+
+function generateBorderButtons() {
+  const buttonName = ['fire', 'water', 'earth'];
+  for (let index = 0; index < 3; index += 1){
+    const newButton = document.createElement('button');
+    newButton.id = buttonName[index];
+    newButton.className = buttonName[index];
+    newButton.style.backgroundImage = `url(./imgs/${buttonName[index]}.jpg)`;
+    document.querySelector('.other-buttons-container').appendChild(newButton);
+  }
+}
+
+generateBorderButtons();
+
+
+function mandaMemetoContainer(self){
+  document.querySelector('#meme-image').removeAttribute('src');
+  document.querySelector('#meme-image').style.backgroundImage = self.target.style.backgroundImage;
+}
+
+
+
+function generateExamples() {
+  
+  for (let index = 0; index < 4; index += 1){
+    const miniMeme  = document.createElement ('div');
+    miniMeme.id = `meme-${index+1}`;
+    miniMeme.style.backgroundImage = `url(./imgs/meme${index+1}.png)`;
+    miniMeme.addEventListener('click',mandaMemetoContainer);
+    document.querySelector('.exampleContainer').appendChild(miniMeme);
+  }
+}
+
+generateExamples();
