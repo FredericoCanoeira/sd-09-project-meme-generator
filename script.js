df7148dd261b878221e466c8ccec95881e9971af
danielfasanaro <danielfasanaro@gmail.com>
2020-12-13 16:42:06 -0300

    3rd Requiriment - Added image container border

--- script.js
+++ script.js
@@ -15,38 +15,38 @@ function addListerners() {
   imgFile.addEventListener('input', changeMemeImg);
 }
 
-function changeElementsStyle() {
+function addStyles() {
   const memeImgContainer = document.querySelector('#meme-image-container');
-  memeImgContainer.style.width = '200px';
-  memeImgContainer.style.height = '200px';
-  // memeImgContainer.style.border = '1px black solid';
+  memeImgContainer.style.width = '300px';
+  memeImgContainer.style.height = '300px';
+  memeImgContainer.style.border = '1px black solid';
   memeImgContainer.style.backgroundColor = 'white';
 
   const memeImgDiv = document.querySelector('#meme-image');
-  memeImgDiv.style.backgroundRepeat = 'no-repeat';
-  memeImgDiv.style.backgroundSize = '100% 100%';
   memeImgDiv.style.width = '100%';
   memeImgDiv.style.height = '100%';
-  memeImgDiv.style.border = '1px black solid';
-
+  memeImgDiv.style.backgroundRepeat = 'no-repeat';
+  memeImgDiv.style.backgroundSize = '100% 100%';
 
   const memeText = document.querySelector('#meme-text');
   memeText.style.top = '70%';
   memeText.style.position = 'relative';
   memeText.style.width = '90%';
-  memeText.style.height = '40px';
+  memeText.style.height = '3em';
   memeText.style.wordBreak = 'break-all';
   memeText.style.overflow = 'hidden';
+  memeText.style.margin = 0;
 }
 
 function localTestFunction() {
   const memeImgDiv = document.querySelector('#meme-image');
   const imgPath = 'url(https://image.shutterstock.com/image-vector/trendy-gradient-liquid-background-lines-260nw-1375011146.jpg)';
   memeImgDiv.style.backgroundImage = imgPath;
+
 }
 
 window.onload = function () {
   addListerners();
-  changeElementsStyle();
+  addStyles();
   // localTestFunction();
 };
