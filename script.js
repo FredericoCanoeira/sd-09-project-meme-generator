8be4fa6976f96115c685dde37e268565d335ef78
danielfasanaro <danielfasanaro@gmail.com>
2020-12-13 16:17:06 -0300

    3rd Requiriment - Added image container border

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ function changeMemeText(event) {
 }
 
 function changeMemeImg(event) {
-  const memeImgContainer = document.querySelector('#meme-image-container');
   const memeImgDiv = document.querySelector('#meme-image');
   memeImgDiv.style.background = `url(${event.target.value})`;
 }
@@ -12,11 +11,25 @@ function changeMemeImg(event) {
 function addListerners() {
   const textInput = document.querySelector('#text-input');
   textInput.addEventListener('input', changeMemeText);
-  const imgFile = document.querySelector('#meme-insert')
+  const imgFile = document.querySelector('#meme-insert');
   imgFile.addEventListener('input', changeMemeImg);
 }
 
 function changeElementsStyle() {
+  const memeImgContainer = document.querySelector('#meme-image-container');
+  memeImgContainer.style.width = '200px';
+  memeImgContainer.style.height = '200px';
+  // memeImgContainer.style.border = '1px black solid';
+  memeImgContainer.style.backgroundColor = 'white';
+
+  const memeImgDiv = document.querySelector('#meme-image');
+  memeImgDiv.style.backgroundRepeat = 'no-repeat';
+  memeImgDiv.style.backgroundSize = '100% 100%';
+  memeImgDiv.style.width = '100%';
+  memeImgDiv.style.height = '100%';
+  memeImgDiv.style.border = '1px black solid';
+
+
   const memeText = document.querySelector('#meme-text');
   memeText.style.top = '70%';
   memeText.style.position = 'relative';
@@ -24,19 +37,10 @@ function changeElementsStyle() {
   memeText.style.height = '40px';
   memeText.style.wordBreak = 'break-all';
   memeText.style.overflow = 'hidden';
-
-  const memeImgContainer = document.querySelector('#meme-image-container');
-  memeImgContainer.style.width = '200px';
-  memeImgContainer.style.height = '200px';
-
-  const memeImgDiv = document.querySelector('#meme-image');
-  memeImgDiv.style.backgroundRepeat = 'no-repeat';
-  memeImgDiv.style.backgroundSize = '100% 100%';
-  memeImgDiv.style.width = '100%';
-  memeImgDiv.style.height = '100%';
 }
 
 function localTestFunction() {
+  const memeImgDiv = document.querySelector('#meme-image');
   const imgPath = 'url(https://image.shutterstock.com/image-vector/trendy-gradient-liquid-background-lines-260nw-1375011146.jpg)';
   memeImgDiv.style.backgroundImage = imgPath;
 }
