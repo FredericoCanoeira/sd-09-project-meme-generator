c275008be60df1ecba7a4d6ef745256eef770dbd
johnealves <johne_af@hotmail.com>
2020-12-14 18:16:38 -0300

    rewrite functions

--- scritp.js
+++ scritp.js
@@ -1,4 +1,5 @@
 const textInput = document.getElementById('text-input');
+const inputMeme = document.getElementById('meme-insert');
 const textMeme = document.getElementById('meme-text');
 const image = document.getElementById('meme-image');
 const imageContainer = document.getElementById('meme-image-container');
@@ -9,26 +10,44 @@ function memetext() {
   });
 }
 
-function loadFile(event) {
-  image.src = URL.createObjectURL(event.target.files[0]);
+function loadFile() {
+  inputMeme.addEventListener('change', function () {
+    image.src = URL.createObjectURL(inputMeme.files[0]);
+  });
 }
 
 function borderFire() {
-  imageContainer.style.border = '3px dashed red';
+  const fire = document.getElementById('fire');
+  fire.addEventListener('click', function () {
+    imageContainer.style.border = '3px dashed red';
+  });
 }
 
 function borderWater() {
-  imageContainer.style.border = '5px double blue';
+  const water = document.getElementById('water');
+  water.addEventListener('click', function () {
+    imageContainer.style.border = '5px double blue';
+  });
 }
 
 function borderEarth() {
-  imageContainer.style.border = '6px groove green';
+  const earth = document.getElementById('earth');
+  earth.addEventListener('click', function () {
+    imageContainer.style.border = '6px groove green';
+  });
 }
 
 function addImageContainer() {
-  image.src = event.target.src;
+  const secMeme = document.getElementById('memes-picture')
+  secMeme.addEventListener('click', function (event) {
+    image.src = event.target.src;
+  });
 }
 
 // chamadas
 memetext();
-
+addImageContainer();
+loadFile();
+borderFire();
+borderWater();
+borderEarth();
