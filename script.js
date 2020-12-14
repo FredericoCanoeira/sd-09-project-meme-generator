4ea96f482c66c43b8ba9c5a1d36e45facf767d23
rach-vp <picanco.raquel@gmail.com>
2020-12-14 17:34:36 -0400

    Implementa propriedades do texto e docontainer

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 function insertTextEvent() {
   const inputBox = document.querySelector('#text-input');
   inputBox.addEventListener('change', function () {
-    const memeText = document.querySelector('#meme-text');
-    memeText.innerText = inputBox.value;
+    const container = document.querySelector('#meme-image-container');
+    if (document.querySelector('#meme-text')) {
+      const oldText = document.querySelector('#meme-text')
+      container.removeChild(oldText);
+    }
+    const newText = document.createElement('p');
+    newText.innerText = inputBox.value;
+    newText.id = 'meme-text';
+    newText.className = 'text-style';
+    container.appendChild(newText);
   });
 }
 
@@ -11,8 +19,17 @@ insertTextEvent();
 function insertImageEvent () {
   const inputImage = document.querySelector('#meme-insert');
   inputImage.addEventListener('change', function () {
-    const memeimage = document.querySelector('#meme-image');
-    memeimage.src = URL.createObjectURL(inputImage.files[0]);
+    const container = document.querySelector('#meme-image-container');
+    if (document.querySelector('#meme-image')) {
+      const oldImage = document.querySelector('#meme-image');
+      container.removeChild(oldImage);
+    }
+    const newImage = document.createElement('img');
+    newImage.id = 'meme-image';
+    newImage.className = 'meme-image-style';
+    newImage.src = URL.createObjectURL(inputImage.files[0]);
+    newImage.alt = 'Meme image';
+    container.appendChild(newImage);
   });
 }
 
