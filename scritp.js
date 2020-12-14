6e3888c685c66bc9918cbc9e3d57da6c844bc2ff
johnealves <johne_af@hotmail.com>
2020-12-14 13:00:36 -0300

    add image input

--- scritp.js
+++ scritp.js
@@ -1,11 +1,17 @@
-const textInput = document.getElementById('text-input')
+const textInput = document.getElementById('text-input');
 const textMeme = document.getElementById('meme-text');
+const imageContainer = document.getElementById('meme-image');
+const memeInsert = document.getElementById('meme-insert');
 
 function memetext() {
   textInput.addEventListener('keyup', function () {
     textMeme.innerHTML = textInput.value;
   });
 }
+
+const loadFile = function (event) {
+  imageContainer.src = URL.createObjectURL(event.target.files[0]);
+};
 
 // chamadas
 memetext();
