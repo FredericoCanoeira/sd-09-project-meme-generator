b88b3a6e40e1251bbf77e0ed3736fe15f05f949d
rach-vp <picanco.raquel@gmail.com>
2020-12-14 18:14:24 -0400

    Implemente botões de customização de bordas

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+// Inserindo texto no container
 function insertTextEvent() {
   const inputBox = document.querySelector('#text-input');
   inputBox.addEventListener('change', function () {
     const container = document.querySelector('#meme-image-container');
     if (document.querySelector('#meme-text')) {
-      const oldText = document.querySelector('#meme-text')
+      const oldText = document.querySelector('#meme-text');
       container.removeChild(oldText);
     }
     const newText = document.createElement('p');
@@ -16,7 +17,8 @@ function insertTextEvent() {
 
 insertTextEvent();
 
-function insertImageEvent () {
+// Inserindo imagem no container
+function insertImageEvent() {
   const inputImage = document.querySelector('#meme-insert');
   inputImage.addEventListener('change', function () {
     const container = document.querySelector('#meme-image-container');
@@ -34,3 +36,36 @@ function insertImageEvent () {
 }
 
 insertImageEvent();
+
+// Evento do botão do fogo
+const fireBtn = document.querySelector('#fire');
+fireBtn.addEventListener('click', function () {
+  const container = document.querySelector('#meme-image-container');
+  if (container.className.includes('fire-border')) {
+    container.className = container.className.replace('fire-border', 'black-border');
+  } else {
+    container.className = container.className.replace('black-border', 'fire-border');
+  }
+});
+
+// Evento do botão da água
+const waterBtn = document.querySelector('#water');
+waterBtn.addEventListener('click', function () {
+  const container = document.querySelector('#meme-image-container');
+  if (container.className.includes('water-border')) {
+    container.className = container.className.replace('water-border', 'black-border');
+  } else {
+    container.className = container.className.replace('black-border', 'water-border');
+  }
+});
+
+// Evento do botão da terra
+const earthBtn = document.querySelector('#earth');
+earthBtn.addEventListener('click', function () {
+  const container = document.querySelector('#meme-image-container');
+  if (container.className.includes('earth-border')) {
+    container.className = container.className.replace('earth-border', 'black-border');
+  } else {
+    container.className = container.className.replace('black-border', 'earth-border');
+  }
+});
