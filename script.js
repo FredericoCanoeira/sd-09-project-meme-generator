24a5433ab6f0ce6b86a2f666154ec45f34b526ba
saulokirchmaier <saulokirchmaier@gmail.com>
2020-12-15 20:48:07 -0300

    Adiciona imagem de meme

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
-
 function getMemeText(event) {
   const memeText = document.querySelector('#meme-text');
   memeText.innerHTML = event.target.value;
 };
 
-function listeners() {
-  let textInput = document.querySelector('#text-input');
-  textInput.addEventListener('input', getMemeText);  
+function getMemeImage(event) {
+  const imageContainer = document.querySelector('#meme-image');
+  imageContainer.src = event.target.value;
+  // imageContainer.style.backgroundImage = "url('https://img.r7.com/images/meme-sorriso-forcado-hide-the-pain-harold-maurice-andras-arato-08112019141225682')";
 }
 
+function listeners() {
+  const textInput = document.querySelector('#text-input');
+  textInput.addEventListener('input', getMemeText);
+  const memeImage = document.querySelector('#meme-insert');
+  memeImage.addEventListener('input', getMemeImage);
+}
   
 window.onload = function () {
   listeners();
