fc80dc7d87c48fffc2f94f31ce6054c772725e86
rach-vp <picanco.raquel@gmail.com>
2020-12-14 21:19:59 -0400

    Implementa escolha de bases pré definidas

--- script.js
+++ script.js
@@ -38,34 +38,70 @@ function insertImageEvent() {
 insertImageEvent();
 
 // Evento do botão do fogo
-const fireBtn = document.querySelector('#fire');
-fireBtn.addEventListener('click', function () {
-  const container = document.querySelector('#meme-image-container');
-  if (container.className.includes('fire-border')) {
-    container.className = 'black-border';
-  } else {
-    container.className = 'fire-border';
-  }
-});
+function fireBtnEven() {
+  const fireBtn = document.querySelector('#fire');
+  fireBtn.addEventListener('click', function () {
+    const container = document.querySelector('#meme-image-container');
+    if (container.className.includes('fire-border')) {
+      container.className = 'black-border';
+    } else {
+      container.className = 'fire-border';
+    }
+  });
+}
+
+fireBtnEven();
 
 // Evento do botão da água
-const waterBtn = document.querySelector('#water');
-waterBtn.addEventListener('click', function () {
-  const container = document.querySelector('#meme-image-container');
-  if (container.className.includes('water-border')) {
-    container.className = 'black-border';
-  } else {
-    container.className = 'water-border';
-  }
-});
+function waterBtnEvent() {
+  const waterBtn = document.querySelector('#water');
+  waterBtn.addEventListener('click', function () {
+    const container = document.querySelector('#meme-image-container');
+    if (container.className.includes('water-border')) {
+      container.className = 'black-border';
+    } else {
+      container.className = 'water-border';
+    }
+  });
+}
+
+waterBtnEvent();
 
 // Evento do botão da terra
-const earthBtn = document.querySelector('#earth');
-earthBtn.addEventListener('click', function () {
-  const container = document.querySelector('#meme-image-container');
-  if (container.className.includes('earth-border')) {
-    container.className = 'black-border';
-  } else {
-    container.className = 'earth-border';
+function earthBtnEvent() {
+  const earthBtn = document.querySelector('#earth');
+  earthBtn.addEventListener('click', function () {
+    const container = document.querySelector('#meme-image-container');
+    if (container.className.includes('earth-border')) {
+      container.className = 'black-border';
+    } else {
+      container.className = 'earth-border';
+    }
+  });
+}
+
+earthBtnEvent();
+
+// Evento de escolher base
+function chooseBase() {
+  const memeBases = document.querySelectorAll('.img-wrap');
+  for (let index = 0; index < memeBases.length; index += 1) {
+    memeBases[index].addEventListener('click', function (event) {
+      console.log('click')
+      const chosenBase = event.target;
+      const mainContainer = document.querySelector('#meme-image-container');
+      if (document.querySelector('#meme-image')) {
+        const oldImage = document.querySelector('#meme-image');
+        mainContainer.removeChild(oldImage);
+      }
+      const newImage = document.createElement('img');
+      newImage.src = chosenBase.src;
+      newImage.alt = chosenBase.alt;
+      newImage.id = 'meme-image';
+      newImage.className = 'meme-image-style';
+      mainContainer.appendChild(newImage);
+    });
   }
-});
+}
+
+chooseBase();
