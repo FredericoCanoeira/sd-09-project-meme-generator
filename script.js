15b5bbc89990c1d60b601195bfeb59c409a79a3f
Felipe Cardoso <felipedclc@gmail.com>
2020-12-16 17:33:16 -0300

    requisito 2

--- script.js
+++ script.js
@@ -1,29 +1,50 @@
-function createInput() {
+function createInputTxt() {
   const input = document.createElement('input');
   input.type = 'text';
   input.id = 'text-input'
   input.placeholder = 'Escreva o meme aqui'
-  const inputForm = document.querySelector('#input-area');
+  const inputForm = document.querySelector('#text');
   inputForm.appendChild(input);
 }
-createInput();
+createInputTxt();
 
-function createContainerImg() {
-  const img = document.createElement('img');
+function createContainerTxtImg() {
   const container = document.querySelector('#meme-image-container');
-  container.appendChild(img);
-  const input = document.querySelector('#text-input');
+  const inputTxt = document.querySelector('#text-input');
 
-  input.addEventListener('keypress', function (e) {
+  inputTxt.addEventListener('keypress', function (e) {
     if (e.keyCode === 13) {
       e.preventDefault();
-      if (input.value.length > 0) {
+      if (inputTxt.value.length > 0) {
         const p = document.createElement('p');
         p.id = 'meme-text';
-        p.innerText = input.value;
+        p.innerText = inputTxt.value;
         container.appendChild(p);
       }
     }
   });
 }
-createContainerImg();
+createContainerTxtImg();
+
+function uploadImage() {
+  const container = document.querySelector('#meme-image-container');
+  const img = document.createElement('img');
+  img.id = 'meme-image';
+  img.style.marginTop = '15px'
+  container.appendChild(img);
+
+  const inputForm = document.querySelector('#img');
+  const inputImg = document.createElement('input');
+  inputImg.type = 'file';
+  inputImg.id = 'meme-insert';
+  inputImg.style.marginTop = '5px';
+  inputForm.appendChild(inputImg);
+
+  inputImg.addEventListener('change', function (e) {
+    // https://www.youtube.com/watch?v=xqIVrjKhfaU
+    if (this.files && this.files[0]) {
+      img.src = URL.createObjectURL(this.files[0]);
+    }
+  });
+}
+uploadImage();
