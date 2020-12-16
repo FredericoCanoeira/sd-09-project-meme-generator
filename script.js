4fb42941a263febfa264b1dffaa67c908548f84e
Jose Daniel Arreaza <jdaniell.93@gmail.com>
2020-12-16 20:10:31 -0300

    second requeriment

--- script.js
+++ script.js
@@ -8,3 +8,11 @@ function addText() {
 }
 
 addText();
+
+var loadFile = function(event) {
+    var output = document.getElementById('output');
+    output.src = URL.createObjectURL(event.target.files[0]);
+    output.onload = function() {
+      URL.revokeObjectURL(output.src) 
+    }
+  };
