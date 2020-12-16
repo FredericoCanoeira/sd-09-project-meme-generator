9d85dcf941ebb4463ffb468e911270a931230292
Jose Daniel Arreaza <jdaniell.93@gmail.com>
2020-12-16 20:20:55 -0300

    second requeriment

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function addText() {
 addText();
 
 var loadFile = function(event) {
-    var output = document.getElementById('output');
+    var output = document.getElementById('meme-image');
     output.src = URL.createObjectURL(event.target.files[0]);
     output.onload = function() {
       URL.revokeObjectURL(output.src) 
