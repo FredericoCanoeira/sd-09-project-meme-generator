b96b5e6719454f152dd74ad7c69d1fc2116d0080
Natalia Negreiros <nataliaknegres@gmail.com>
2020-12-17 19:00:34 -0300

    [Req 7] Adiciona função

--- script.js
+++ script.js
@@ -3,10 +3,12 @@
 const textInput = document.querySelector('#text-input');
 const fileInput = document.querySelector('#meme-insert');
 const textImage = document.querySelector('#meme-text');
+
 const btnFire = document.querySelector('#fire');
 const btnWater = document.querySelector('#water');
 const btnEarth = document.querySelector('#earth');
 
+const containerReady = document.querySelector('.ready');
 
 // Functions | Funções
 
@@ -16,27 +18,44 @@ function insertTextImage() {
 }
 
 function insertImage(event) {
-  let img = document.querySelector('#meme-image');
+  const img = document.querySelector('#meme-image');
   img.src = URL.createObjectURL(event.target.files[0]);
   img.onload = function () {
     URL.revokeObjectURL(img.src); // free memory
-  }
+  };
 }
 
 function fireBorder() {
-  let imgContainer = document.querySelector('#meme-image-container');
+  const imgContainer = document.querySelector('#meme-image-container');
   imgContainer.style.border = '3px dashed red';
 }
 
 function waterBorder() {
-  let imgContainer = document.querySelector('#meme-image-container');
+  const imgContainer = document.querySelector('#meme-image-container');
   imgContainer.style.border = '5px double blue';
 }
 
 function earthBorder() {
-  let imgContainer = document.querySelector('#meme-image-container');
+  const imgContainer = document.querySelector('#meme-image-container');
   imgContainer.style.border = '6px groove green';
 }
+
+function imageSelect() {
+  const imgContainer = document.querySelector('#meme-image-container');
+  const img = document.querySelector('#meme-image');
+  const imgs = document.querySelectorAll('.img-ready');
+  for (let index = 0; index < imgs.length; index += 1) {
+    imgs[index].addEventListener('click', function (event) {
+      if (img.src) {
+        img.removeAttribute('src');
+        img.src = event.target.src;
+      } else {
+        img.src = event.target.src;
+      }
+    })
+  }
+}
+imageSelect();
 
 // Event Listeners | Eventos
 
