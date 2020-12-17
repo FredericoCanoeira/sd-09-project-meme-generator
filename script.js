b809fe86a2ab820a6487373a5b3611f6c22ea134
Felipe Cardoso <felipedclc@gmail.com>
2020-12-17 14:07:57 -0300

    mudancas projeto

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function createInputTxt() {
   const input = document.createElement('input');
   input.type = 'text';
-  input.id = 'text-input'
-  input.placeholder = 'Escreva o meme aqui'
+  input.id = 'text-input';
+  input.placeholder = 'Escreva o meme aqui';
   const inputForm = document.querySelector('#text');
   inputForm.appendChild(input);
 }
@@ -26,6 +26,28 @@ function createContainerTxtImg() {
 }
 createContainerTxtImg();
 
+function createBtnDeleteTxt() {
+  const btn = document.createElement('button');
+  btn.id = 'delete-text';
+  btn.innerText = 'apaga texto';
+  btn.style.marginLeft = '5px';
+  btn.style.padding = '3.5px'
+  const btnPai = document.querySelector('#text');
+  btnPai.appendChild(btn);
+
+  btn.addEventListener('click', function (e) {
+    const p = document.querySelector('#meme-text');
+    const paiP = document.querySelector('#meme-image-container');
+    if (p) {
+      e.preventDefault();
+      const inputTxt = document.querySelector('#text-input');
+      inputTxt.value = '';
+      paiP.removeChild(p);
+    }
+  });
+}
+createBtnDeleteTxt();
+
 function uploadImage() {
   const container = document.querySelector('#meme-image-container');
   const img = document.createElement('img');
@@ -48,3 +70,25 @@ function uploadImage() {
   });
 }
 uploadImage();
+
+function btnMarginColor(color, text) {
+  const btnColor = document.createElement('button');
+  btnColor.innerText = text;
+  btnColor.style.marginLeft = '5px';
+  btnColor.style.marginTop = '10px'
+  btnColor.style.padding = '3.5px';
+  btnColor.style.background = color;
+  btnColor.style.color = 'white';
+  const btnColorPai = document.querySelector('#margin-colors');
+  btnColorPai.appendChild(btnColor);
+
+  btnColor.addEventListener('click', function (e) {
+    const container = document.querySelector('#meme-image-container');
+    container.style.borderColor = color;
+  });
+}
+btnMarginColor('red', 'Borda vermelha');
+btnMarginColor('blue', 'Borda azul');
+btnMarginColor('green', 'Borda verde');
+btnMarginColor('yellow', 'Borda amarela');
+btnMarginColor('black', 'Original');
