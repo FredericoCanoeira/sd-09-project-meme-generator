df997ce3ee50ad6c689593ee652158e5d11dcace
Felipe Rocha <felipe.sr27@gmail.com>
2020-12-17 18:10:47 -0300

    Requirement 2: Created functions that create and delete paragraph in HTML.

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-// Prevent submit of a input in form
-function preventSubmit(event) {
-  event.preventDefault();
-}
-
+// Create element img in HTML
 function createImg(event) {
   const imgContainer = document.querySelector('#meme-image-container');
   const imgElement = document.createElement('img');
@@ -14,11 +10,40 @@ function createImg(event) {
   }
 }
 
+// Create element paragraph(p) in HTML
+function createParagraph(text, container) {
+  const paragraph = document.createElement('p');
+  paragraph.innerHTML = text;
+  paragraph.id = 'meme-paragraph';
+  container.appendChild(paragraph);
+}
+
+// Delete element paragraph(p) in HTML
+function deleteParagraph(container) {
+  if (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+}
+
+// Call functions that create/delete paragraph in HTML
+function loadText(event) {
+  const textInput = event.target;
+  const textContainer = document.querySelector('#meme-text');
+  deleteParagraph(textContainer);
+  createParagraph(textInput.value, textContainer);
+}
+
+// Prevent submit of a input in form
+function preventSubmit(event) {
+  event.preventDefault();
+}
 
 // Load functions and event listeners at the window load  
 window.onload = function () {
-  const textInput = document.querySelector('#text-input');
+  const textForm = document.querySelector('#text-form');
   const memeInsert = document.querySelector('#meme-insert');
-  textInput.addEventListener('submit', preventSubmit);
+  const textInput = document.querySelector('#text-input')
+  textForm.addEventListener('submit', preventSubmit);
+  textInput.addEventListener('keyup', loadText);
   memeInsert.addEventListener('input', createImg);
 };
