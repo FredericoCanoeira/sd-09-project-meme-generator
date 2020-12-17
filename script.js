67389b6a984fc2be86aea4be2e4b5379ba59d1cb
Natalia Negreiros <nataliaknegres@gmail.com>
2020-12-17 15:12:39 -0300

    [Req 1] Adiciona função para inserir texto

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 // Selectors | Seletores
+const textInput = document.querySelector('#text-input');
+const fileInput = document.querySelector('#meme-insert');
+const textImage = document.querySelector('#meme-text');
 
 
 // Event Listeners | Eventos
-
+textInput.addEventListener('input', insertTextImage);
 
 // Functions | Funções
+
+function insertTextImage() {
+  const text = textInput.value;
+  textImage.innerText = text;
+}
