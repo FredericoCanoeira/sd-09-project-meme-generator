bc19f345809bf21f23001d64fdcf8a2e5da86e8c
Jose Daniel Arreaza <jdaniell.93@gmail.com>
2020-12-17 10:57:51 -0300

    memes container

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const output = document.getElementById('meme-image');
+
 function addText() {
     let textInput = document.querySelector('#text-input');
     let textBox = document.querySelector('#meme-text');
@@ -10,9 +12,21 @@ function addText() {
 addText();
 
 var loadFile = function(event) {
-    var output = document.getElementById('meme-image');
+    
     output.src = URL.createObjectURL(event.target.files[0]);
     output.onload = function() {
       URL.revokeObjectURL(output.src) 
     }
   };
+
+
+  function loadMeme() {
+   let memeList = document.querySelectorAll('.memes');
+   for (let i = 0; i < memeList.length; i++) {
+     memeList[i].addEventListener('click', (event) => {
+       output.src = `${event.target.src}`
+       console.log(event.target.src)    
+     })
+   }
+  }
+  loadMeme();
