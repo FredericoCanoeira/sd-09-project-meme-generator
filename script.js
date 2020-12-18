f7de82dc43e9e4f8e866676d1a557b6b33c8030a
Renato Feitosa <pythaqua@gmail.com>
2020-12-18 04:53:59 -0300

    add 7th requirement - ready made memes

--- script.js
+++ script.js
@@ -4,21 +4,53 @@ const memeInsert = document.getElementById('meme-insert');
 const memeImage = document.getElementById('meme-image');
 const memeText = document.getElementById('meme-text');
 const borderSettings = document.getElementById('border-settings');
+const readyMadeMemes = document.getElementById('ready-made-memes');
+const memeImages = {
+  'meme-1': 'meme1',
+  'meme-2': 'meme2',
+  'meme-3': 'meme3',
+  'meme-4': 'meme4',
+};
 
 function insertMemeText() {
   const text = textInput.value;
   memeText.innerText = text;
 }
 
+function changeMemeImage(imageURL) {
+  memeImage.style.backgroundImage = `url('${imageURL}')`;
+}
+
 function insertMemeImage(event) {
   // Reference: https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded/27165977#27165977
   const image = URL.createObjectURL(event.target.files[0]);
-  memeImage.style.backgroundImage = `url('${image}')`;
+  changeMemeImage(image);
 }
 
 function changeBorder(event) {
-  const eventClassName = event.target.id;
-  memeContainer.className = `meme-image-container ${eventClassName}-border`;
+  if (event.target.classList.contains('button')) {
+    const eventClassName = event.target.id;
+    memeContainer.className = `meme-image-container ${eventClassName}-border`;
+  }
+}
+
+function getImage(idName) {
+  return `imgs/${memeImages[idName]}.png`;
+}
+
+function insertReadyMemeImage(event) {
+  if (event.target.classList.contains('thumbnail')) {
+    const idName = event.target.id;
+    changeMemeImage(getImage(idName));
+  }
+}
+
+function setReadyMadeMemesImages() {
+  const thumbsDivs = readyMadeMemes.children;
+  for (let index = 0; index < thumbsDivs.length; index += 1) {
+    const idName = thumbsDivs[index].id;
+    thumbsDivs[index].style.backgroundImage = `url('${getImage(idName)}')`;
+  }
 }
 
 function setMemeInsertEvents() {
@@ -33,8 +65,14 @@ function setBorderSettingsEvents() {
   borderSettings.addEventListener('click', changeBorder);
 }
 
+function setReadyMadeMemesEvents() {
+  readyMadeMemes.addEventListener('click', insertReadyMemeImage);
+}
+
 window.onload = function () {
   setMemeInsertEvents();
   setTextInputEvents();
   setBorderSettingsEvents();
+  setReadyMadeMemesImages();
+  setReadyMadeMemesEvents();
 };
