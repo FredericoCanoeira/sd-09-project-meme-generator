46522b7c76ef64c32ea48c0d553b85bfe165339f
Christopher <chris.tryber@gmail.com>
2020-12-18 17:39:56 -0300

    Requisito 1 segunda tentativa

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//let memeImageContainer = document.querySelector('#meme-image-container')
+let memeImageContainer = document.querySelector('#meme-image-container');
+let memeText = document.querySelector('#meme-text');
+let textInput = document.querySelector('#text-input');
+
+textInput.addEventListener('keyup', function (){
+    memeText.innerText = textInput.value;
+})
