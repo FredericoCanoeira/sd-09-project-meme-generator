47db20b01f64d0fb65ca74ef67ec289dc020a26f
marcusWittho <witthoeft.mcs@hotmail.com>
2020-12-21 10:48:20 -0300

    Meme Generator correções climate

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ const memeText = document.querySelector('#meme-text');
 
 textInput.addEventListener('input', () => {
   memeText.innerText = textInput.value.toUpperCase();
-})
+});
 textInput.addEventListener('keypress', (tecla) => {
   if (tecla.keyCode === 13) {
-    textInput.value = ''
+    textInput.value = '';
     textInput.focus();
   }
-})
+});
 
-let loadFile = function(event) {
-  let memeImage = document.querySelector('#meme-image');
+const loadFile = function (event) {
+  const memeImage = document.querySelector('#meme-image');
   memeImage.src = URL.createObjectURL(event.target.files[0]);
-  memeImage.onload = function() {
-    URL.revokeObjectURL(memeImage.src)
+  memeImage.onload = function () {
+    URL.revokeObjectURL(memeImage.src);
   }
 };
