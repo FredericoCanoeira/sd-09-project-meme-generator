d0182ac368f5fef4fc2e6d0938422490acb99b1f
Daniela Muller <danielamuller20@gmail.com>
2020-12-21 16:17:27 -0300

    Upload imagem meme

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const input = document.querySelector('#text-input');
 const memeContainer = document.querySelector('#meme-image-container');
 const memeText = document.querySelector('#meme-text');
+const memeImages = document.querySelector('#meme-image');
+const inputFile = document.querySelector('#meme-insert');
 
 function memePhrase() {
   input.addEventListener('input', function () {
@@ -8,3 +10,18 @@ function memePhrase() {
   });
 }
 memePhrase();
+
+// SOURCE:
+// https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded
+function memeImage() {
+  inputFile.addEventListener('change', function (event)  {
+    const image = document.createElement('img');
+    image.src = URL.createObjectURL(event.target.files[0]);
+    image.className = 'meme-image';
+    memeImages.appendChild(image);
+    image.addEventListener('load', function () {
+      URL.revokeObjectURL(image.src);
+    });
+  });
+}
+  memeImage();
