448486fcf83d1d4ed5e02d5e8b50ea5176eb0b31
Pablo Fernandes <pabloprates@gmail.com>
2020-12-21 15:40:48 -0300

    Requirement One - Main containers

--- script.js
+++ script.js
@@ -3,5 +3,12 @@ function setImageToContainer() {
     imageContainer.src = URL.createObjectURL(fileInput.files[0]);
 }
 
+function setTextToImage() {
+    const textContent = document.querySelector('.meme-text');
+    textContent.innerText = document.getElementById('text-input').value;
+}
+
+const inputTextButton = document.getElementById('add-text');
+inputTextButton.addEventListener('click', setTextToImage);
 const fileInput = document.getElementById('meme-insert');
 fileInput.addEventListener('change', setImageToContainer);
