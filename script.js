f334dfd0736b1dd865291545a6f859195f8159c6
marcusWittho <witthoeft.mcs@hotmail.com>
2020-12-22 12:03:11 -0300

    Meme Generator requisito 7

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const textInput = document.querySelector('#text-input');
 const memeText = document.querySelector('#meme-text');
 const memeInsert = document.querySelector('#meme-insert');
-const btnFire = document.querySelector('#fire')
+const btnFire = document.querySelector('#fire');
 const btnWater = document.querySelector('#water');
 const btnEarth = document.querySelector('#earth');
 const memeImageContainer = document.querySelector('#meme-image-container');
-const buttons = document.querySelectorAll('button');
+const memeImage = document.querySelector('#meme-image');
+const preMadeMeme = document.querySelector('.pre-made-meme');
 
 textInput.addEventListener('input', () => {
   memeText.innerText = textInput.value.toUpperCase();
@@ -18,7 +19,6 @@ textInput.addEventListener('keypress', (tecla) => {
 });
 
 memeInsert.addEventListener('change', (event) => {
-  const memeImage = document.querySelector('#meme-image');
   memeImage.src = URL.createObjectURL(event.target.files[0]);
   memeImage.onload = function () {
     URL.revokeObjectURL(memeImage.src);
@@ -26,13 +26,21 @@ memeInsert.addEventListener('change', (event) => {
 });
 
 btnFire.addEventListener('click', () => {
-  (memeImageContainer.style.border !== '3px dashed red') ? memeImageContainer.style.border = '3px dashed red' : memeImageContainer.style.border = '1px solid black';
-})
+  return (memeImageContainer.style.border !== '3px dashed red') ? memeImageContainer.style.border = '3px dashed red' : memeImageContainer.style.border = '1px solid black';
+});
 
 btnWater.addEventListener('click', () => {
-  (memeImageContainer.style.border !== '5px double blue') ? memeImageContainer.style.border = '5px double blue' : memeImageContainer.style.border = '1px solid black';
-})
+  return (memeImageContainer.style.border !== '5px double blue') ? memeImageContainer.style.border = '5px double blue' : memeImageContainer.style.border = '1px solid black';
+});
 
 btnEarth.addEventListener('click', () => {
-  (memeImageContainer.style.border !== '6px groove green') ? memeImageContainer.style.border = '6px groove green' : memeImageContainer.style.border = '1px solid black';
-})
+  return (memeImageContainer.style.border !== '6px groove green') ? memeImageContainer.style.border = '6px groove green' : memeImageContainer.style.border = '1px solid black';
+});
+
+preMadeMeme.addEventListener('click', (event) => {
+  for (let index = 1; index <= 4; index += 1) {
+    if (event.target.className === `meme-${index}`) {
+      memeImage.src = `imgs/meme-${[index]}.png`;
+    }
+  }
+});
