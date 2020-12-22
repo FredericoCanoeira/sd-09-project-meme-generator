42d0448e987814e8f23c6e8a0ecce1123d83ca1a
marcusWittho <witthoeft.mcs@hotmail.com>
2020-12-22 15:10:38 -0300

    Meme Generator correções

--- script.js
+++ script.js
@@ -12,12 +12,12 @@ const preMadeMeme = document.querySelector('.pre-made-meme');
 textInput.addEventListener('input', () => {
   memeText.innerText = textInput.value.toUpperCase();
 });
-textInput.addEventListener('keypress', (tecla) => {
-  if (tecla.keyCode === 13) {
-    textInput.value = '';
-    textInput.focus();
-  }
-});
+// textInput.addEventListener('keypress', (tecla) => {
+//   if (tecla.keyCode === 13) {
+//     textInput.value = '';
+//     textInput.focus();
+//   }
+// });
 
 memeInsert.addEventListener('change', (event) => {
   memeImage.src = URL.createObjectURL(event.target.files[0]);
@@ -41,6 +41,7 @@ memeInsert.addEventListener('change', (event) => {
 changeBorder.addEventListener('click', (event) => {
   const classes = ['fire', 'water', 'earth'];
   const classesValue = ['3px dashed red', '5px double blue', '6px groove green'];
+
   for (let index = 0; index < classes.length; index += 1) {
     if (memeImageContainer.style.border === classesValue[index]) {
       memeImageContainer.style.border = '1px solid black';
@@ -53,7 +54,7 @@ changeBorder.addEventListener('click', (event) => {
 preMadeMeme.addEventListener('click', (event) => {
   for (let index = 1; index <= 4; index += 1) {
     if (event.target.id === `meme-${index}`) {
-      memeImage.src = 'imgs/meme-'+[index]+'.png';
+      memeImage.src = `imgs/meme${[index]}.png`;
     }
   }
 });
