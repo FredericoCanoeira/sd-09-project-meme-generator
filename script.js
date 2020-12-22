341d7ce7dd7068fb98d66edf2e426ff5b4b8cbb0
Rafael Azevedo <rafaelazevedo321@gmail.com>
2020-12-22 19:51:43 -0300

    feito requisito 2

--- script.js
+++ script.js
@@ -4,7 +4,14 @@ function showText() {
   paragraph.innerText = text.value;
 }
 
+function insertImage() {
+  const image = document.querySelector('#meme-image');
+  image.src = 'Imagens/cavera.jpg';
+}
+
 window.onload = function () {
   const text = document.querySelector('#text-input');
   text.addEventListener('keyup', showText);
+  const btnInsertImage = document.querySelector('#meme-insert');
+  btnInsertImage.addEventListener('click', insertImage);
 };
