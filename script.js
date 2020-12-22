51c1594d85a1f174b9ad7fb0492d60a19c2a15c8
marcusWittho <witthoeft.mcs@hotmail.com>
2020-12-21 21:43:20 -0300

    Meme Generator requisito 6

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const textInput = document.querySelector('#text-input');
 const memeText = document.querySelector('#meme-text');
 const memeInsert = document.querySelector('#meme-insert');
+const btnFire = document.querySelector('#fire')
+const btnWater = document.querySelector('#water');
+const btnEarth = document.querySelector('#earth');
+const memeImageContainer = document.querySelector('#meme-image-container');
+const buttons = document.querySelectorAll('button');
 
 textInput.addEventListener('input', () => {
   memeText.innerText = textInput.value.toUpperCase();
@@ -12,10 +17,22 @@ textInput.addEventListener('keypress', (tecla) => {
   }
 });
 
-memeInsert.addEventListener("change", (event) => {
+memeInsert.addEventListener('change', (event) => {
   const memeImage = document.querySelector('#meme-image');
   memeImage.src = URL.createObjectURL(event.target.files[0]);
   memeImage.onload = function () {
     URL.revokeObjectURL(memeImage.src);
   };
 });
+
+btnFire.addEventListener('click', () => {
+  (memeImageContainer.style.border !== '3px dashed red') ? memeImageContainer.style.border = '3px dashed red' : memeImageContainer.style.border = '1px solid black';
+})
+
+btnWater.addEventListener('click', () => {
+  (memeImageContainer.style.border !== '5px double blue') ? memeImageContainer.style.border = '5px double blue' : memeImageContainer.style.border = '1px solid black';
+})
+
+btnEarth.addEventListener('click', () => {
+  (memeImageContainer.style.border !== '6px groove green') ? memeImageContainer.style.border = '6px groove green' : memeImageContainer.style.border = '1px solid black';
+})
