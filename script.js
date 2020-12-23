ab008736628b49d0f6b7806ff3366bb54230d18a
Leticia Lima <letylima96@gmail.com>
2020-12-23 02:12:33 -0300

    realiza desafio 2

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const input = document.querySelector('#text-input');
+const inputImage = document.querySelector('#meme-insert');
 
 function showText() {
   const memeText = document.querySelector('#meme-text');
   memeText.innerHTML = input.value;
 }
 input.addEventListener('input', showText);
+
+function uploadImage() {
+  const memeImage = document.querySelector('#meme-image');
+  memeImage.src = inputImage.value;
+  console.log(memeImage.src);
+}
+inputImage.addEventListener('input', uploadImage);
