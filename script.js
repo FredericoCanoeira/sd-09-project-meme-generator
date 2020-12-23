dc5446371b59063c1e3ce77fc025285935fe6938
Leticia Lima <letylima96@gmail.com>
2020-12-23 01:28:20 -0300

    conserta o desafio 1

--- script.js
+++ script.js
@@ -3,6 +3,5 @@ const input = document.querySelector('#text-input');
 function showText() {
   const memeText = document.querySelector('#meme-text');
   memeText.innerHTML = input.value;
-  input.value = '';
 }
-input.addEventListener('change', showText);
+input.addEventListener('input', showText);
