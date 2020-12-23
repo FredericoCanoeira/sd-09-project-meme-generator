bd6c54ff30da6bfa51910bf93bf71d15dd1d7fde
Gabriel Castro <kastro.gabriel17@gmail.com>
2020-12-23 03:25:19 -0400

    set css

--- script.js
+++ script.js
@@ -6,35 +6,34 @@ const memeContainer = document.querySelector('#meme-image-container');
 const getImg = document.querySelector('.meme-image');
 const memeImgExample = document.querySelector('#meme-example');
 memeContainer.style.border = '1px solid black';
+inputText.setAttribute('maxLength', 60);
 
 function changeTextMeme() {
   inputText.addEventListener('input', () => (memeText.innerHTML = inputText.value));
 }
 
-function changeImgMeme(){
+function changeImgMeme() {
     inputFile.addEventListener('change', (event) => (memeImg.src = URL.createObjectURL(event.target.files[0])))
 }
 
-function backgroundImgMeme(urlImage) {
-    memeContainer.style.backgroundImg = `url(${urlImage})`;
+function backgroundImgMeme(urlImg) {
+    memeContainer.style.backgroundImg = `url(${urlImg})`;
 }
 
-function fireButton(){
+function fireButton() {
     const fire = document.querySelector('#fire');
     fire.addEventListener('click', () => (memeContainer.style.border = '3px dashed red'))
 }
 
-function waterButton(){
+function waterButton() {
     const water = document.querySelector('#water');
     water.addEventListener('click', () => (memeContainer.style.border = '5px double blue'))
 }
 
-function earthButton(){
+function earthButton() {
     const earth = document.querySelector('#earth');
     earth.addEventListener('click', () => (memeContainer.style.border = '3px groove green'))
 }
-
-
 
 changeTextMeme();
 changeImgMeme();
