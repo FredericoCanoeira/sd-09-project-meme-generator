86f547c9d56cccda47d0e3a237f0e1ba97cef62e
Rafael Azevedo <rafaelazevedo321@gmail.com>
2020-12-23 15:38:55 -0300

    feito requisito 7 bonus

--- script.js
+++ script.js
@@ -27,4 +27,24 @@ window.onload = function () {
   btnEarth.addEventListener('click', function () {
     container.className = 'earth';
   });
+  const meme1 = document.querySelector('#meme-1');
+  meme1.addEventListener('click', function () {
+    const image = document.querySelector('#meme-image');
+    image.src = 'imgs/meme1.png'
+  });
+  const meme2 = document.querySelector('#meme-2');
+  meme2.addEventListener('click', function () {
+    const image = document.querySelector('#meme-image');
+    image.src = 'imgs/meme2.png'
+  });
+  const meme3 = document.querySelector('#meme-3');
+  meme3.addEventListener('click', function () {
+    const image = document.querySelector('#meme-image');
+    image.src = 'imgs/meme3.png'
+  });
+  const meme4 = document.querySelector('#meme-4');
+  meme4.addEventListener('click', function () {
+    const image = document.querySelector('#meme-image');
+    image.src = 'imgs/meme4.png'
+  });
 };
