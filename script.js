9cb6b2394c87e5299a5ddd54586ae6c1b7a936a3
RodrigoNeoEx <draja.acc@gmail.com>
2020-12-25 11:05:58 -0300

    add listener to auto input innerText on <p>

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
 function createStructure () {
   const apend = document.getElementById('meme-image-container');
+  
   const createTitle = document.createElement('h1');
   createTitle.innerText = 'Meme Generator'
   apend.appendChild(createTitle);
+
   const createInput = document.createElement('input');
   createInput.id = 'text-input';
+  createInput.type = 'text';
   apend.appendChild(createInput);
-  const memeText = document.createElement('p');
-  memeText.innerText = 'teste';
+
+  const memeText = document.createElement('p');  
+  memeText.id = 'meme-text';
   apend.appendChild(memeText);
+
+  const imageContainer = document.createElement('input');
+  imageContainer.id = 'meme-image';
+  imageContainer.type = 'file'
+  apend.appendChild(imageContainer);
 }
 
+function inputText () {
+  const textInputed = document.getElementById('text-input');
+  let textContainer = document.getElementById('meme-text');
+  if(textInputed.value !== '') {
+    textContainer.innerText = textInputed.value;
+  }
+}
 
+function listeners () {
+  const textContainer = document.getElementById('text-input');
+  textContainer.addEventListener('keyup', inputText);  
+}
 
 window.onload = function() {
-  createStructure()
+  createStructure();
+  listeners ();
+  inputText();
 }
