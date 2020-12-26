c78d3ed0a62e6a03b3bf66923fc9881b52c91547
Rafa Reis <rafaelrnascimento2006@gmail.com>
2020-12-26 14:59:23 -0300

    primeiros requisitos feitos

---
+++ script.js
@@ -0,0 +1,20 @@
+function generateMemeText() {
+  const textInput = document.querySelector('#text-input');
+  textInput.addEventListener('keyup', function(event) {
+    const memeText = document.querySelector('#meme-text');
+      memeText.innerText = textInput.value;
+  });
+}
+
+function generateMemeImage() {
+  const fileInput = document.querySelector('#meme-insert');
+  fileInput.addEventListener('change', function (memeImage) {
+    const image = document.querySelector('#meme-image');
+    image.src = URL.createObjectURL(memeImage.target.files[0]);
+  });
+}
+
+window.onload = function () {
+  generateMemeText();
+  generateMemeImage();
+};
