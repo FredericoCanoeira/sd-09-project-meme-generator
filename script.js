91499bae69cbcaee0cce26c5d88fc284082f8155
Rafa Reis <rafaelrnascimento2006@gmail.com>
2020-12-26 16:16:28 -0300

    requisito 6 feito

--- script.js
+++ script.js
@@ -14,7 +14,31 @@ function generateMemeImage() {
   });
 }
 
+function addFireBorder() {
+  const memeContainer = document.querySelector('#meme-image-container');
+  let memeBorder = memeContainer.style.border;
+  memeContainer.style.border = '3px dashed red';
+}
+
+function addWaterBorder() {
+  const memeContainer = document.querySelector('#meme-image-container');
+  memeContainer.style.border = '5px double blue';
+}
+
+function addEarthBorder() {
+  const memeContainer = document.querySelector('#meme-image-container');
+  memeContainer.style.border = '6px groove green';
+}
+
 window.onload = function () {
+  const fireButton = document.querySelector('#fire');
+  const waterButton = document.querySelector('#water');
+  const earthButton = document.querySelector('#earth');
+
+  fireButton.addEventListener('click', addFireBorder);
+  waterButton.addEventListener('click', addWaterBorder);
+  earthButton.addEventListener('click', addEarthBorder);
+
   generateMemeText();
   generateMemeImage();
 };
