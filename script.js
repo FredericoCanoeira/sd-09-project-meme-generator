c8e62588773910212ec8af0073ee35fae53cd460
Khyrki <khyrkimitsunobi@gmail.com>
2020-12-27 20:37:38 -0300

    troca a funcao click pela funcao inout

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 const inputBox = document.querySelector('#text-input');
 const addTextButton = document.querySelector('#ad-text')
-const textMemeDiv = document.querySelector('#meme-text')
+const textMeme = document.querySelector('#meme-text')
 
 function addTextToMeme() {
-  addTextButton.addEventListener('click', function() {
+  inputBox.addEventListener('input', function() {
     const text = inputBox.value;
-    const pMemeText = textMemeDiv.children[0]
-    pMemeText.innerHTML = text;
-    inputBox.value = '';
+    textMeme.innerHTML = text;
+    
   });
 }
 addTextToMeme()
