61be1c519fe33813d8798a8dfb3b06c5e1ee738a
LucioOSilva <lucioosilva00@gmail.com>
2020-12-28 17:02:40 -0300

    style.css refactored and removed reset.css

--- script.js
+++ script.js
@@ -2,21 +2,17 @@ function writeText() {
   const textInput = document.querySelector('#text-input');
   const paragraphText = document.querySelector('#meme-text');
   textInput.addEventListener('input', function (event) {
-    paragraphText.innerText = event.target.value
+    paragraphText.innerText = event.target.value;
   });
 }
 
-function uploadImage () {
+function uploadImage() {
   const imageInput = document.querySelector('#meme-insert');
   const imageUp = document.querySelector('#meme-image');
-  imageInput.addEventListener('change', function (event) {    
+  imageInput.addEventListener('change', function (event) {
     imageUp.src = URL.createObjectURL(event.target.files[0]);
   });
 }
 
-
-
-
-
-uploadImage()
-writeText()
+uploadImage();
+writeText();
