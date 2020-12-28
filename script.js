d49c53531972850ea6cd8e65db387065b455097c
RodrigoNeoEx <draja.acc@gmail.com>
2020-12-28 12:07:15 -0300

    add imgs, add function do memeButtons but still by class need to work on it

--- script.js
+++ script.js
@@ -29,30 +29,51 @@ function createStructure () {
   img.src = '';
   document.getElementById('meme-image').appendChild(img);
 
-  const buttonSection = document.createElement('section');
-  buttonSection.id = 'allButtons';
-  document.body.appendChild(buttonSection);
+  const buttonMemesSection = document.createElement('section');
+  buttonMemesSection.id = 'allMemes';
+  document.body.appendChild(buttonMemesSection);
+
+  const buttonBorderSection = document.createElement('section');
+  buttonBorderSection.id = 'allBorderButtons';
+  document.body.appendChild(buttonBorderSection);
 
   const buttonFire = document.createElement('button');
-  buttonFire.id = 'fire';
-  buttonFire.className = 'fire';
+  buttonFire.id = 'fire';  
   buttonFire.style.backgroundColor = 'red';
-  document.getElementById('allButtons').appendChild(buttonFire);
+  document.getElementById('allBorderButtons').appendChild(buttonFire);
 
   const buttonWater = document.createElement('button');
-  buttonWater.id = 'water';
-  buttonWater.className = 'water';
+  buttonWater.id = 'water';  
   buttonWater.style.backgroundColor = 'blue';
-  document.getElementById('allButtons').appendChild(buttonWater);
+  document.getElementById('allBorderButtons').appendChild(buttonWater);
 
   const buttonEarth = document.createElement('button');
-  buttonEarth.id = 'earth';
-  buttonEarth.className = 'earth';
+  buttonEarth.id = 'earth';  
   buttonEarth.style.backgroundColor = 'green';
-  document.getElementById('allButtons').appendChild(buttonEarth);
+  document.getElementById('allBorderButtons').appendChild(buttonEarth);
+
+  const buttonMeme1 = document.createElement('button');
+  buttonMeme1.id = 'meme-1';
+  buttonMeme1.className = 'meme-1';
+  document.getElementById('allMemes').appendChild(buttonMeme1);
+
+  const buttonMeme2 = document.createElement('button');
+  buttonMeme2.id = 'meme-2';
+  buttonMeme2.className = 'meme-2';
+  document.getElementById('allMemes').appendChild(buttonMeme2);
+
+  const buttonMeme3 = document.createElement('button');
+  buttonMeme3.id = 'meme-3';
+  buttonMeme3.className = 'meme-3';
+  document.getElementById('allMemes').appendChild(buttonMeme3);
+
+  const buttonMeme4 = document.createElement('button');
+  buttonMeme4.id = 'meme-4';
+  buttonMeme4.className = 'meme-4';
+  document.getElementById('allMemes').appendChild(buttonMeme4);
 }
 
-function inputText () {
+function inputText() {
   const textInputed = document.getElementById('text-input');
   let textContainer = document.getElementById('meme-text');
   if(textInputed.value !== '') {
@@ -60,7 +81,7 @@ function inputText () {
   }
 }
 
-function loadFile(event) {  
+function loadFile(event) {
   let output = document.getElementById('output');
   if(output !== '') {
     output.src = URL.createObjectURL(event.target.files[0]);
@@ -72,9 +93,9 @@ function loadFile(event) {
 }
 
 function buttonFire() {
-  const fire = document.getElementById('meme-image-container');  
+  const fire = document.getElementById('meme-image-container');
   if (fire.classList.value.includes('fire')) {
-    fire.classList.remove('fire');    
+    fire.classList.remove('fire');
   } else {
     fire.classList.add('fire');
     fire.classList.remove('water', 'earth');
@@ -84,7 +105,7 @@ function buttonFire() {
 function buttonWater() {
   const water = document.getElementById('meme-image-container');  
   if (water.classList.value.includes('water')) {
-    water.classList.remove('water');    
+    water.classList.remove('water');
   } else {
     water.classList.add('water');
     water.classList.remove('fire', 'earth');
@@ -92,32 +113,79 @@ function buttonWater() {
 }
 
 function buttonEarth() {
-  const earth = document.getElementById('meme-image-container');  
+  const earth = document.getElementById('meme-image-container');
   if (earth.classList.value.includes('earth')) {
-    earth.classList.remove('earth');    
+    earth.classList.remove('earth');
   } else {
     earth.classList.add('earth');
     earth.classList.remove('fire', 'water');
   }
 }
 
-function listeners () {
+function setMeme1() {  
+  let loadMeme1 = document.getElementById('output');  
+  if (loadMeme1.classList.value.includes('meme-1')) {
+    loadMeme1.classList.remove('meme-1');
+  } else {
+    loadMeme1.classList.add('meme-1');
+    loadMeme1.classList.remove('meme-2', 'meme-3', 'meme-4');
+  }
+}
+
+
+function setMeme2() {  
+  let loadMeme2 = document.getElementById('output');
+  if (loadMeme2.classList.value.includes('meme-2')) {
+    loadMeme2.classList.remove('meme-2');
+  } else {
+    loadMeme2.classList.add('meme-2');
+    loadMeme2.classList.remove('meme-1', 'meme-3', 'meme-4');
+  }
+}
+
+function setMeme3() {  
+  let loadMeme3 = document.getElementById('output');
+  if (loadMeme3.classList.value.includes('meme-3')) {
+    loadMeme3.classList.remove('meme-3');
+  } else {
+    loadMeme3.classList.add('meme-3');
+    loadMeme3.classList.remove('meme-1', 'meme-2', 'meme-4');
+  }
+}
+
+function setMeme4() {  
+  let loadMeme4 = document.getElementById('output');
+  if (loadMeme4.classList.value.includes('meme-4')) {
+    loadMeme4.classList.remove('meme-4');
+  } else {
+    loadMeme4.classList.add('meme-4');
+    loadMeme4.classList.remove('meme-2', 'meme-3', 'meme-1');
+  }
+}
+
+function listeners() {
   const textContainer = document.getElementById('text-input');
   textContainer.addEventListener('keyup', inputText);
   const insertImage = document.getElementById('meme-insert');
   insertImage.addEventListener('change', loadFile);
   const eventButtonFire = document.getElementById('fire');
   eventButtonFire.addEventListener('click', buttonFire);
-
   const eventButtonWater = document.getElementById('water');
   eventButtonWater.addEventListener('click', buttonWater);
-
   const eventButtonEarth = document.getElementById('earth');
   eventButtonEarth.addEventListener('click', buttonEarth);
+  const firstButtonMeme = document.getElementById('meme-1');
+  firstButtonMeme.addEventListener('click', setMeme1);
+  const secondButtonMeme = document.getElementById('meme-2');
+  secondButtonMeme.addEventListener('click', setMeme2);
+  const thirdButtonMeme = document.getElementById('meme-3');
+  thirdButtonMeme.addEventListener('click', setMeme3);
+  const fourthButtonMeme = document.getElementById('meme-4');
+  fourthButtonMeme.addEventListener('click', setMeme4);
 }
 
 window.onload = function() {
   createStructure();
   listeners ();
   inputText();  
-}
+};
