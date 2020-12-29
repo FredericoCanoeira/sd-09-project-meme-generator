70a30ca5e850bfe296457a0a45d0b8dd0e136329
Leticia Lima <letylima96@gmail.com>
2020-12-29 06:30:57 -0300

    ajusta issues code climate

--- script.js
+++ script.js
@@ -12,16 +12,8 @@ function showText() {
 }
 input.addEventListener('input', showText);
 
-function localTestFunction() {
-  const memeImgDiv = document.querySelector('#meme-image');
-  memeImgDiv.src = 'https://image.shutterstock.com/image-vector/trendy-gradient-liquid-background-lines-260nw-1375011146.jpg';
-  console.log(memeImgDiv.src);
-}
-// localTestFunction();
-
 function uploadImage() {
   const memeImage = document.querySelector('#meme-image');
-  // memeImage.style.backgroundImage = URL.createObjectURL(inputImage.value);
   memeImage.style.backgroundImage = `url(${inputImage.value})`;
 }
 inputImage.addEventListener('input', uploadImage);
