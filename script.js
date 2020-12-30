b2731cc02220668f5514c4c23866811781988958
Thiago Carreira Vallim <thiagocvallim@gmail.com>
2020-12-29 23:40:51 -0400

    Adiciona requisito 2

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const input = document.querySelector('.input');
+const memeText = document.querySelector('#meme-text');
+const insertImage = document.querySelector('#meme-insert');
+const meme = document.querySelector('#meme-image');
 
 function setTextMeme() {
-  const memeText = document.querySelector('#meme-text');
   memeText.innerText = input.value;
+}
+
+function changeMeme(event) {
+  meme.src = URL.createObjectURL(event.target.files[0]);
 }
 
 function setEvent() {
   input.addEventListener('keyup', setTextMeme);
+  insertImage.addEventListener('change', changeMeme);
 }
 
 setEvent();
