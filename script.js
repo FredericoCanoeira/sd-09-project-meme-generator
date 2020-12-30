c9ce7a662192c426a17d3bef6cae8df72e2a8998
Thiago Carreira Vallim <thiagocvallim@gmail.com>
2020-12-30 02:04:37 -0400

    Adiciona requisitos 5

--- script.js
+++ script.js
@@ -2,9 +2,8 @@ const input = document.querySelector('.input');
 const memeText = document.querySelector('#meme-text');
 const insertImage = document.querySelector('#meme-insert');
 const meme = document.querySelector('#meme-image');
-const fire = document.querySelector('#fire');
-const water = document.querySelector('#water');
-const earth = document.querySelector('#earth');
+const buttons = document.querySelector('.buttons');
+const memeContainer = document.querySelector('#meme-image-container');
 
 function setTextMeme() {
   memeText.innerText = input.value;
@@ -14,9 +13,26 @@ function changeMeme(event) {
   meme.src = URL.createObjectURL(event.target.files[0]);
 }
 
+function borderClick(event) {
+  if (event.target.className !== 'buttons') {
+    memeContainer.className = '';
+    memeContainer.classList.add('mainImage')
+    if(event.target.id === 'fire'){
+      memeContainer.classList.add('fire');
+    }
+    if(event.target.id === 'water'){
+      memeContainer.classList.add('water');
+    }
+    if(event.target.id === 'earth'){
+      memeContainer.classList.add('earth');
+    }
+  }
+}
+
 function setEvent() {
   input.addEventListener('keyup', setTextMeme);
   insertImage.addEventListener('change', changeMeme);
+  buttons.addEventListener('click', borderClick);
 }
 
 setEvent();
