867b7d73a91cc8f4240cde3122f584fa91a73d31
Thiago Carreira Vallim <thiagocvallim@gmail.com>
2020-12-30 02:37:57 -0400

    Adiciona requisito 7

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const insertImage = document.querySelector('#meme-insert');
 const meme = document.querySelector('#meme-image');
 const buttons = document.querySelector('.buttons');
 const memeContainer = document.querySelector('#meme-image-container');
+const imgs = document.querySelector('.imgContainer');
 
 function setTextMeme() {
   memeText.innerText = input.value;
@@ -13,19 +14,29 @@ function changeMeme(event) {
   meme.src = URL.createObjectURL(event.target.files[0]);
 }
 
+function compareClass(event) {
+  if(event.target.id === 'fire'){
+    memeContainer.classList.add('fire');
+  }
+  if(event.target.id === 'water'){
+    memeContainer.classList.add('water');
+  }
+  if(event.target.id === 'earth'){
+    memeContainer.classList.add('earth');
+  }
+}
+
 function borderClick(event) {
   if (event.target.className !== 'buttons') {
     memeContainer.className = '';
     memeContainer.classList.add('mainImage')
-    if(event.target.id === 'fire'){
-      memeContainer.classList.add('fire');
-    }
-    if(event.target.id === 'water'){
-      memeContainer.classList.add('water');
-    }
-    if(event.target.id === 'earth'){
-      memeContainer.classList.add('earth');
-    }
+    compareClass(event);
+  }
+}
+
+function setPreMemes(event) {
+  if (!event.target.classList.contains('imgContainer')) {
+    meme.src = event.target.src;
   }
 }
 
@@ -33,6 +44,7 @@ function setEvent() {
   input.addEventListener('keyup', setTextMeme);
   insertImage.addEventListener('change', changeMeme);
   buttons.addEventListener('click', borderClick);
+  imgs.addEventListener('click', setPreMemes);
 }
 
 setEvent();
