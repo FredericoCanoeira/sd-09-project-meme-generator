3b055a7ebbe043e2a063a21739c579eb83a75382
Bernardo Smith <bernardostech@gmail.com>
2020-12-30 17:38:08 -0500

    construindo o codigo2

--- script.js
+++ script.js
@@ -7,3 +7,25 @@
     texto.innerHTML = message.value.toUpperCase();    
 
  }
+
+let button1 = document.querySelector('#fire');
+let button2 = document.querySelector('#water');
+let button3 = document.querySelector('#earth');
+let image = document.querySelector("#blah");
+button1.addEventListener('click', adicionaBorda1);
+button2.addEventListener('click', adicionaBorda2);
+button3.addEventListener('click', adicionaBorda3);
+
+
+
+function adicionaBorda1(){
+   image.style.border = '3px dashed red'
+}
+
+function adicionaBorda2(){
+   image.style.border = '5px double blue'
+}
+
+function adicionaBorda3(){
+   image.style.border = '6px groove green'
+}
