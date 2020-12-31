171fd84ea810f81911addf9e763ebb3a87d5fbb5
brenno-calado <brenno.calado@gmail.com>
2020-12-30 21:17:56 -0300

    Task 2

--- script.js
+++ script.js
@@ -4,9 +4,26 @@ function insertText() {
   memeText.innerText = textInput.value;
 }
 
+function displayMeme() {
+  const memeInsert = document.querySelector('#meme-insert');
+  const memeImage = document.querySelector('#meme-image');
+  // Remove all Child from div if there's any child. Used code at https://developer.mozilla.org/en-US/docs/Web/API/Node/removeChild
+  while (memeImage.firstChild) {
+    memeImage.removeChild(memeImage.firstChild);
+  }
+  const img = document.createElement('img');
+  img.src = URL.createObjectURL(memeInsert.files[0]);
+  img.style.width = '500px';
+  img.style.height = '500px';
+  memeImage.appendChild(img);
+}
+// calcular a proporção da imagem para depois multiplicar o lado maior que tomará o tamanho de 500px pela proporção calculada
+
 window.onload = function () {
   const textInput = document.querySelector('#text-input');
   textInput.addEventListener('keyup', insertText);
+  const memeInsert = document.querySelector('#meme-insert');
+  memeInsert.addEventListener('change', displayMeme);
 };
 
 // limite máximo de caracteres 60: An element with position: absolute; is positioned relative t
