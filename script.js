d228f0d4c0faac91e458dae8f4bd0b4d57b8c293
Bernardo Smith <bernardostech@gmail.com>
2020-12-30 21:20:50 -0500

    construindo o codigo4

--- script.js
+++ script.js
@@ -11,7 +11,7 @@
 let button1 = document.querySelector('#fire');
 let button2 = document.querySelector('#water');
 let button3 = document.querySelector('#earth');
-let image = document.querySelector("#blah");
+let image = document.querySelector(".container");
 button1.addEventListener('click', adicionaBorda1);
 button2.addEventListener('click', adicionaBorda2);
 button3.addEventListener('click', adicionaBorda3);
@@ -29,3 +29,30 @@ function adicionaBorda2(){
 function adicionaBorda3(){
    image.style.border = '6px groove green'
 }
+/*
+let foto1 = document.querySelector('#meme-1');
+let foto2 = document.querySelector('#meme-2');
+let foto3 = document.querySelector('#meme-3');
+let foto4 = document.querySelector('#meme-4');
+let image = document.querySelector("#meme-image" );
+button1.addEventListener('click', fotos1);
+button2.addEventListener('click', fotos2);
+button3.addEventListener('click', fotos3);
+button4.addEventListener('click', fotos4);
+
+
+function fotos1(){
+   image.src = foto1.src
+}
+
+function fotos2(){
+   image.src = '5px double blue'
+}
+
+function fotos3(){
+   image.src = '6px groove green'
+}
+
+function fotos4(){
+   image.src = '6px groove green'
+}*/
