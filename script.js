39120d32ba851c274ca3d3d846f4b29166e97112
brenno-calado <brenno.calado@gmail.com>
2020-12-31 11:06:31 -0300

    Task bonus 6

--- script.js
+++ script.js
@@ -17,7 +17,32 @@ function displayMeme() {
   img.style.height = '500px';
   memeImage.appendChild(img);
 }
-// calcular a proporção da imagem 
+
+function changeBorder(evt) {
+  const clickedButtonId = evt.target.id;
+  const memeImageContainer = document.querySelector('#meme-image-container');
+  switch(clickedButtonId) {
+    case 'default-border':
+      memeImageContainer.classList.remove('fire', 'earth', 'water');
+      memeImageContainer.classList.add('default-border');
+      break;
+    case 'fire':
+      memeImageContainer.classList.remove('default-border', 'earth', 'water');
+      memeImageContainer.classList.add('fire');
+      break;
+    case 'earth':
+      memeImageContainer.classList.remove('default-border', 'fire', 'water');
+      memeImageContainer.classList.add('earth');
+      break;
+    case 'water':
+      memeImageContainer.classList.remove('default-border', 'fire', 'earth');
+      memeImageContainer.classList.add('water');
+      break;
+    default:
+      //do nothing;
+  }
+}
+// calcular a proporção da imagem
 // multiplicar o lado maior que tomará o tamanho de 500px pela proporção calculada
 
 window.onload = function () {
@@ -25,6 +50,8 @@ window.onload = function () {
   textInput.addEventListener('keyup', insertText);
   const memeInsert = document.querySelector('#meme-insert');
   memeInsert.addEventListener('change', displayMeme);
+  const buttonContainer = document.querySelector('#button-container');
+  buttonContainer.addEventListener('click', changeBorder);
 };
 
 // limite máximo de caracteres 60: An element with position: absolute; is positioned relative t
