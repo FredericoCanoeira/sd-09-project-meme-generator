c2ac6c605651d4cdcad52d82598a454f99277c66
Enio Nicael <enionicael@gmail.com>
2020-12-31 01:35:53 -0300

    Feature: requisito 6 adiciona 3 botoes que alteram a borda do meme.

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
 const inputContainer = document.getElementById('input-container');
 const memeContainer = document.getElementById('meme-image-container');
+const buttonsContainer = document.querySelector('.buttons-container');
 
 function handleUpdateValue(event) {
   const text = document.getElementById('meme-text');
-  const inputText = event.target.value;
-  console.log(inputText.length);
-  if (inputText.length < 60) {
-    text.innerText = event.target.value;
-  } else {
-    alert('Entrada máxima é de 60 caracteres.');
-  }
+  text.innerText = event.target.value;
 }
 
 const generateInputText = function () {
@@ -18,6 +13,7 @@ const generateInputText = function () {
   labelElement.innerText = 'Texto:';
   const inputTextElement = document.createElement('input');
   inputTextElement.id = 'text-input';
+  inputTextElement.maxLength = '60';
   inputTextElement.addEventListener('input', handleUpdateValue);
   inputContainer.appendChild(labelElement);
   inputContainer.appendChild(inputTextElement);
@@ -53,9 +49,55 @@ const generateMemeText = function () {
   memeContainer.appendChild(pElement);
 };
 
+const generateButton = function (id, value) {
+  const button = document.createElement('button');
+  button.id = id;
+  button.className = 'btn';
+  button.innerText = value;
+  return button;
+};
+
+function handleChangeFireBorder() {
+  console.log(memeContainer);
+  memeContainer.style.border = '3px dashed red';
+}
+
+const generateButtonFire = function () {
+  const buttonFire = generateButton('fire', 'Fire');
+  buttonFire.addEventListener('click', handleChangeFireBorder);
+  buttonsContainer.appendChild(buttonFire);
+};
+
+function handleChangeWaterBorder() {
+  memeContainer.style.border = '5px double blue';
+}
+
+const generateButtonWater = function () {
+  const buttonWater = generateButton('water', 'Water');
+  buttonWater.addEventListener('click', handleChangeWaterBorder);
+  buttonsContainer.appendChild(buttonWater);
+};
+
+function handleChangeEarthBorder() {
+  memeContainer.style.border = '6px groove green';
+}
+
+const generateButtonEarth = function () {
+  const buttonEarth = generateButton('earth', 'Earth');
+  buttonEarth.addEventListener('click', handleChangeEarthBorder);
+  buttonsContainer.appendChild(buttonEarth);
+};
+
 window.onload = function () {
   generateInputText();
   generateMemeImage();
   generateMemeText();
   generateInputFile();
+  generateButtonFire();
+  generateButtonWater();
+  generateButtonEarth();
 };
+
+// fire -> &#128293;
+// water -> &#128167
+// earth -> &#129716;
