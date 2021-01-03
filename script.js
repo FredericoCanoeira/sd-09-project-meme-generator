dd73e099163478def4a2678916d715d39becb517
Nathália <nathaliaveneziano@icloud.com>
2021-01-03 20:45:31 -0300

    Correçao Avaliador e Requisito 06

--- script.js
+++ script.js
@@ -8,3 +8,43 @@ text.addEventListener('input', function () {
 document.querySelector('#meme-insert').addEventListener('change', function (event) {
   document.querySelector('#meme-image').src = URL.createObjectURL(event.target.files[0]);
 });
+
+// Bordas
+function removeAllBorderClass(element) {
+  if (element.classList.contains('fire') || element.classList.contains('water') || element.classList.contains('earth')) {
+    element.classList.remove('fire');
+    element.classList.remove('water');
+    element.classList.remove('earth');
+  }
+}
+
+function changeBorder(type) {
+  const element = document.getElementById('meme-image-container');
+  element.style.border = '';
+
+  removeAllBorderClass(element);
+
+  if (type === 'fire') {
+    element.classList.add('fire');
+  }
+
+  if (type === 'water') {
+    element.classList.add('water');
+  }
+
+  if (type === 'earth') {
+    element.classList.add('earth');
+  }
+}
+
+document.querySelector('#fire').addEventListener('click', function () {
+  changeBorder('fire');
+});
+
+document.querySelector('#water').addEventListener('click', function () {
+  changeBorder('water');
+});
+
+document.querySelector('#earth').addEventListener('click', function () {
+  changeBorder('earth');
+});
