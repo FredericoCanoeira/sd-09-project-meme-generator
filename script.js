0335c17a6949ecc47b324bcf886bb6dba6114e01
Caue Lopes <kitoypp@gmail.com>
2021-01-04 13:52:36 -0300

    exercicio 2

--- script.js
+++ script.js
@@ -6,3 +6,26 @@ putText.innerHTML = getText.value;
 }
 
 getText.addEventListener('input', changeText);
+
+
+/*
+const putImage = document.querySelector('#meme-insert');
+
+function changeImage() {
+const getImage = document.querySelector('#meme-image');
+getImage.style.backgroundImage = `url(${putImage.value})`;
+}
+
+
+putImage.addEventListener('input', changeImage);
+*/
+
+
+const putImage = document.querySelector('#meme-insert')
+
+function changeImage(event) {
+  let image = document.getElementById('meme-image');
+  image.src = URL.createObjectURL(event.target.files [0]);
+}
+
+putImage.addEventListener('input', changeImage);
