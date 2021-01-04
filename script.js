4c4f92bc874f5bb81d84c1ef137ef5ff394a6e91
fernandacajueiro <fernandacajueiromachado@gmail.com>
2021-01-04 18:13:52 -0300

    Alterações novas

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+let loadFile = function (event) {
+  let reader = new FileReader();
+  reader.onload = function () {
+    let image = document.getElementById('meme-image');
+    image.src = reader.result;
+  };
+  reader.readAsDataURL(event.target.files[0]);
+}
 
+function getTextInput () {
+  const input = document.getElementById('text-input');
+  const output = document.getElementById('meme-text');
+  input.addEventListener('change', function () {
+    output.innerText = input.value;
+  });
+}
+
+window.onload = function () {
+  getTextInput();
+}
