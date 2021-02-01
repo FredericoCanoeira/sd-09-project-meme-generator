7145346a702738252a78835e3d07fc5ad7bb0a9e
thosijulio <thosijulio@gmail.com>
2021-01-31 23:09:18 -0300

    alteracoes no style.css

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 function inserirTextoMeme () {
   let textInput = document.querySelector ('#text-input');
   textInput.addEventListener('input', function () {
-      let memeText = document.querySelector ('#meme-text');
-      memeText.innerText = textInput.value;
+    let memeText = document.querySelector ('#meme-text');
+    memeText.innerText = textInput.value;
+  })
+}
+
+function inserirImagemMeme () {
+  let memeInsert = document.querySelector ('#meme-insert');
+  memeInsert.addEventListener('change', function () {
+      let memeImage = document.querySelector ('#meme-image');
+      console.log(memeInsert.files[0]);
   })
 }
 
 inserirTextoMeme ();
+inserirImagemMeme ();
