0934768d9bba54c078082effe9a32ff730f62f3c
Matheus Martino <matheus.martino97@gmail.com>
2021-02-17 14:30:49 -0300

    Adding mini ready memes.

--- src/script.js
+++ src/script.js
@@ -1,42 +1,54 @@
-window.onload = windowLoad()
+window.onload = windowLoad();
 
 function listenToTextInput() {
-  const textInput = document.querySelector('#text-input')
-  const memeText = document.querySelector('#meme-text')
+  const textInput = document.querySelector('#text-input');
+  const memeText = document.querySelector('#meme-text');
 
   textInput.addEventListener('keyup', () => {
-      memeText.innerText = textInput.value
-  })
+    memeText.innerText = textInput.value;
+  });
 }
 
 function listenToCustomButtons() {
-  const customButtonsNodeList = document.querySelectorAll('#custom-buttons button')
-  const memeImageContainer = document.querySelector('#meme-image-container')
+  const customButtonsNodeList = document.querySelectorAll(
+    '#custom-buttons button'
+  );
+  const memeImageContainer = document.querySelector('#meme-image-container');
 
   customButtonsNodeList[0].addEventListener('click', () => {
-    memeImageContainer.style.border = '1px double red'
-  })
+    memeImageContainer.style.border = '1px double red';
+  });
 
   customButtonsNodeList[1].addEventListener('click', () => {
-    memeImageContainer.style.border = '1px ridge blue'
-  })
+    memeImageContainer.style.border = '1px ridge blue';
+  });
 
   customButtonsNodeList[2].addEventListener('click', () => {
-    memeImageContainer.style.border = '1px dashed green'
-  })
+    memeImageContainer.style.border = '1px dashed green';
+  });
 }
 
 function listenToMemeInsert() {
-  const memeInsert = document.querySelector('#meme-insert')
-  const image = document.querySelector('#meme-image img')
+  const memeInsert = document.querySelector('#meme-insert');
+  const image = document.querySelector('#meme-image img');
 
   memeInsert.addEventListener('change', () => {
-    image.src = URL.createObjectURL(memeInsert.files[0])
+    image.src = URL.createObjectURL(memeInsert.files[0]);
+  });
+}
+
+function listenToProntos() {
+  const prontos = document.querySelector('#prontos')
+  const image = document.querySelector('#meme-image img');
+
+  prontos.addEventListener('click', (event) => {
+    image.src = event.target.src
   })
 }
 
 function windowLoad() {
-  listenToTextInput()
-  listenToCustomButtons()
-  listenToMemeInsert()
+  listenToTextInput();
+  listenToCustomButtons();
+  listenToMemeInsert();
+  listenToProntos();
 }
