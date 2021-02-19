ab0110f9f8c872460f9a7fc0596a74269de6fbf2
Luciano_Lodi <lrlodi@gmail.com>
2021-02-19 14:44:26 -0300

    Primeiros requisitos

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-document.querySelector('#gen-meme').addEventListener('click', generateMeme);
 const emptyDiv = document.querySelector('#meme-image-container');
 const emptyText = document.querySelector('#meme-text');
 const textToAdd = document.querySelector('#text-input');
@@ -6,8 +5,12 @@ const textToAdd = document.querySelector('#text-input');
 function generateMeme() {
   const uploadedImage = document.querySelector('#meme-image').files[0];
   const image = document.createElement('img');
-  emptyText.innerText = textToAdd.value;
+  const insertedText = document.createElement('p');
+  insertedText.innerText = textToAdd.value;
+  insertedText.className = "inserted-text";
+  emptyText.appendChild(insertedText);
   image.src = URL.createObjectURL(uploadedImage);
-  emptyDiv.appendChild(image);
-
+  emptyDiv.appendChild(image);  
 }
+
+document.querySelector('#gen-meme').addEventListener('click', generateMeme);
