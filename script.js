2e2e0acaa27e233234ce4fe73fb2aee439346f2e
Luciano_Lodi <lrlodi@gmail.com>
2021-02-22 10:22:15 -0300

    Atualizaçao do metodo de gatilho dos eventos para adicionar imagem e texto

--- script.js
+++ script.js
@@ -2,15 +2,20 @@ const emptyDiv = document.querySelector('#meme-image-container');
 const emptyText = document.querySelector('#meme-text');
 const textToAdd = document.querySelector('#text-input');
 
-function generateMeme() {
-  const uploadedImage = document.querySelector('#meme-image').files[0];
-  const image = document.createElement('img');
+function geneMemeText() {
   const insertedText = document.createElement('p');
   insertedText.innerText = textToAdd.value;
   insertedText.className = "inserted-text";
   emptyText.appendChild(insertedText);
+}
+
+function genMemeImage() {
+  const uploadedImage = document.querySelector('#meme-image').files[0];
+  console.log(uploadedImage)
+  const image = document.createElement('img');
   image.src = URL.createObjectURL(uploadedImage);
   emptyDiv.appendChild(image);  
 }
 
-document.querySelector('#gen-meme').addEventListener('click', generateMeme);
+document.querySelector('#meme-image').addEventListener('change', genMemeImage);
+document.querySelector('#text-input').addEventListener('keyup', geneMemeText);
